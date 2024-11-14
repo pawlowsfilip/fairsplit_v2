00134ea679eda83f9c0570835ba3523730edce29
Filip <pawlows.filip@gmail.com>
2024-11-14 15:48:57 +0100

    Added lambda handler

--- aws/lambda_handler.py
+++ aws/lambda_handler.py
@@ -1,1 +1,18 @@
 # AWS Lambda handler functions
+import json
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hello from Lambda!")
+    }
